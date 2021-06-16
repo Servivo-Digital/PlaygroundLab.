c6fe79f1847a8e6e3bc53d019251cbd8360a3ac4
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-15 23:31:48 -0500

    create hero html structure

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,8 +4,48 @@ export default function Hero() {
     return (
         <>
             <section>
-                
-            </section>        
+                <div className="hero-container">
+                    <div className="hero-content">
+                        <div className="hero-img">
+                            <img src="https://i.postimg.cc/pVnRP9T7/Mask-Group.png" alt="niño" />
+                        </div>
+                        <div className="hero-text">
+                            <h1>Comienza en el increíble mundo de la tecnología y sé parte del futuro.</h1>
+                            <div className="hero-card">
+                                <div className="hero-card-container">
+                                    <div className="hero-card-content">
+                                        <div className="hero-card-header">
+                                            <div className="hero-card-header-img">
+                                                <img src="" alt="" />
+                                            </div>
+                                            <div className="hero-card-header-text">
+                                                <p>
+                                                Clase de JavaScript desde 0
+                                                </p>
+                                                <p>
+                                                    <span>
+                                                    mas de 2 horas de contenido  
+                                                    </span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="hero-card-body">
+                                            <div className="hero-card-body-container">
+                                                <div className="hero-card-body-content">
+                                                    <button>
+                                                        Ver Ahora
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
