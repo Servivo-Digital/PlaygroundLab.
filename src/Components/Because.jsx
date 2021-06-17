599780aa7349f253cca48ee45eb42e5f5e7147ca
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-17 09:01:48 -0500

    Quote  mobile first

--- src/Components/Because.jsx
+++ src/Components/Because.jsx
@@ -6,6 +6,7 @@ export default function Because() {
         <section className="Because">
             <div className="Because-container">
                 <div className="Because-content">
+                    <h2 className="Because-title">¿Por qué aprender con nosotros? </h2>
                     <Quote/>
                     <Values/>
                 </div>
