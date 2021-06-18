01fcac3d6729e16b3a493cb6009fd982b1cd65d0
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-18 18:00:08 -0500

    Footer mobile first i need more information

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,34 @@ export default function Footer() {
 
     const socialItems = [
         {
-            name:'',
-            icon:'',
-            url:'',
-        }
+            name: 'Youtube',
+            icon: 'https://i.postimg.cc/pTbVqs86/youtube-1.png',
+            url: 'https://www.youtube.com/channel/UCJHGgOUNJ3ICSNclSYWtqDg',
+
+        },
+        {
+            name: 'Twitter',
+            icon: 'https://i.postimg.cc/tJ9qH7Bd/gorjeo.png',
+            url: '',
+
+        },
+        {
+            name: 'Linkedin',
+            icon: 'https://i.postimg.cc/hPySHDTx/linkedin.png',
+            url: '',
+
+        },
+        {
+            name: 'Instagram',
+            icon: 'https://i.postimg.cc/Wb3VmBC4/instagram.png',
+            url: '',
+
+        },
+        {
+            name: 'Facebook',
+            icon: 'https://i.postimg.cc/j54TvtRD/facebook.png',
+            url: '',
+        },
     ]
 
     return (
@@ -23,11 +47,24 @@ export default function Footer() {
                         </div>
                     </div>
                     <div className="Footer-footer">
-                        <div className="Footer-footer-copy">
-
-                        </div>
                         <div className="Footer-footer-social">
-
+                            {
+                                socialItems.map((item, index) => (
+                                    item.url == '' ? '' :
+                                        <div key={(index * 100 + "_") + item.name}>
+                                            <div className="icon-img">
+                                                <a href={item.url}>
+                                                    <img src={item.icon} alt={item.name} />    
+                                                </a>
+                                            </div>
+                                            
+                                        </div>
+                                ))
+                            }
+                        </div>
+                        <div className="Footer-footer-copy">
+                            <p>Diseñado con ♥️ en Playground lab</p>
+                            <p className="dir">Perif. Sur 3720, Jardines del Pedregal, Álvaro Obregón, 01900 Ciudad de México, CDMX</p>
                         </div>
                     </div>
                 </div>
@@ -35,3 +72,7 @@ export default function Footer() {
         </footer>
     )
 }
+
+/*
+<div>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+*/
