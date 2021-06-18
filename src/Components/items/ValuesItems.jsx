7af0242c8162477d54b8c5322b8ae8277501beeb
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-18 11:46:44 -0500

    Because mobile first
    +split code in 3 files 
    +add file Quote
    +add file Values 
    +create new folder items in css

--- src/Components/items/ValuesItems.jsx
+++ src/Components/items/ValuesItems.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react' 
 
-export default function ValuesItems({title,imgUrl,description}) {
+export default function ValuesItems({title,imgUrl,description,index}) {
+    
+    const titleIndex= ['one','two','three','four']
     return (
         <div className="ValuesItems-container">
             <div className="ValuesItems-content">
-                <div className="ValuesItems-text">
-                    <h3 className="ValuesItems-title">{title}</h3>
-                    <p className="ValuesItems-description">{description}</p>
-                </div>
                 {
                 imgUrl === '' ?' ':
                     <div className="ValuesItems-img">
                         <img src={imgUrl} alt="" />
                     </div>
                 }
+                <div className="ValuesItems-text"> 
+                    <h3 className={"ValuesItems-title"+" "+ titleIndex[index]}>{title}</h3>
+                    <p className="ValuesItems-description">{description}</p>
+                </div>
             </div>
         </div>
-    )
+    ) 
 }
